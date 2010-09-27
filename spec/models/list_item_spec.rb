7a60430f171a361813c50ae1429adf991d4384a9
Bryan Rehbein <bryan@siliconsea.net>
2010-09-27 15:38:08 -0600

    Added pending stubs for list_item model functionality, fixed minor css bug for header a:visited

--- spec/models/list_item_spec.rb
+++ spec/models/list_item_spec.rb
@@ -2,4 +2,8 @@ require 'spec_helper'
 
 describe ListItem do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  pending "validate ListItem.due_today?"
+  pending "validate ListItem.overdue?"
+  pending "validate shorthand due date in ListItem.name on create"
 end
